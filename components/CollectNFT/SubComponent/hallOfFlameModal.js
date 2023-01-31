65cd7ae0724ff43d265a1106c109293f4d740710
Himanshu-Pandey-04 <himanshuvpandey@gmail.com>
2023-01-31 18:38:49 +0530

    wo finalized OutsideClickHandler

--- components/CollectNFT/SubComponent/hallOfFlameModal.js
+++ components/CollectNFT/SubComponent/hallOfFlameModal.js
@@ -92,26 +92,25 @@ function HallOfFlameModal({
                         })}
                     </div>
 
-                        <OutsideClickHandler
+                      <OutsideClickHandler
                     onOutsideClick={() => {close(); console.log("Done")}}
                   >
                     <div className="flex items-center justify-center">
                       <div className={`modalPrev mr-[60px] `}>
-                        <div
+                    <div
                           className={`flex justify-center items-center cursor-pointer w-[72px] h-[72px] bg-black/60 rounded-[100px] ${styles.swiperArrow}`}
                         >
-
                           <LeftArrow />
                         </div>
                       </div>
                       <Card
                         cardDetails={ele}
                         showCollectModal={() => onCollectNow(ele)}
-                        />
+                      />
                       <div className={`modalNext ml-[60px]`}>
                         <div
                           className={`flex justify-center items-center cursor-pointer w-[72px] h-[72px] bg-black/60 rounded-[100px] ${styles.swiperArrow}`}
-                          >
+                        >
                           <RightArrow />
                         </div>
                       </div>
@@ -123,9 +122,9 @@ function HallOfFlameModal({
                     >
                       <div
                         className={`${styles.upvoteCount} flex items-center justify-center`}
-                        >
+                      >
                         <span className="pr-[10px]">
-                        <Image
+                          <Image
                             src="https://static.plgworks.com/assets/images/non/flame-icon.png"
                             alt="Lens Icon"
                             width="23"
@@ -133,55 +132,55 @@ function HallOfFlameModal({
                           />
                         </span>
                         <span>{ele.totalVotes}</span>
-                        </div>
-                        <div className={`${styles.nftDetails} p-[15px]`}>
+                      </div>
+                      <div className={`${styles.nftDetails} p-[15px]`}>
                         <div className="flex items-start justify-between">
-                        <div className={styles.nftTitle}>{ele.title}</div>
+                          <div className={styles.nftTitle}>{ele.title}</div>
                           <div>
-                          <Image
+                            <Image
                               src="https://static.plgworks.com/assets/images/non/vote/lens-icon.png"
                               alt="Lens icon"
                               width="20"
                               height="20"
-                              />
-                              </div>
-                              </div>
-                              <div className="flex justify-between items-center mt-[14px] mb-[22px]">
+                            />
+                          </div>
+                        </div>
+                        <div className="flex justify-between items-center mt-[14px] mb-[22px]">
                           <div className="flex items-center font-medium text-[#ffffff99] text-[16px] leading-[26px]">
-                          <span>{ele.handle}</span>
-                          <span>.</span>
-                          <span>Follow</span>
+                            <span>{ele.handle}</span>
+                            <span>.</span>
+                            <span>Follow</span>
                           </div>
                           <div className="flex items-center font-medium text-[#ffffff99] text-[16px] leading-[26px]">
-                          <span></span>
-                          <span>Show Prompt</span>
+                            <span></span>
+                            <span>Show Prompt</span>
                           </div>
-                          </div>
-                          {ele.hasCollected ? (
-                            <button
-                            className={`${styles.collectButton} flex items-center justify-center py-[7px]`}
-                            >
-                            <span>
-                            <Collect />
-                            </span>
-                            <span className="font-bold text-[16px] leading-[26px] ml-[8px]">
-                            You have already collected this
-                            </span>
-                            </button>
-                            ) : (
+                        </div>
+                        {ele.hasCollected ? (
                           <button
-                          className={`${styles.collectButton} flex items-center justify-center py-[7px]`}
-                          onClick={() => onCollectNow(ele)}
+                            className={`${styles.collectButton} flex items-center justify-center py-[7px]`}
                           >
                             <span>
-                            <Collect />
+                              <Collect />
+                            </span>
+                            <span className="font-bold text-[16px] leading-[26px] ml-[8px]">
+                              You have already collected this
+                            </span>
+                          </button>
+                        ) : (
+                          <button
+                            className={`${styles.collectButton} flex items-center justify-center py-[7px]`}
+                            onClick={() => onCollectNow(ele)}
+                          >
+                            <span>
+                              <Collect />
                             </span>
                             <span className="font-bold text-[16px] leading-[26px] ml-[8px]">
                               Collect Now
-                              </span>
+                            </span>
                           </button>
-                          )}
-                            </div>*/}
+                        )}
+                      </div>*/}
                     </div> 
                     </OutsideClickHandler>
                   </div>
@@ -191,7 +190,7 @@ function HallOfFlameModal({
         </Swiper>
       </div>
     </div>
-    ) : null;
+  ) : null;
 }
 
 export default HallOfFlameModal;
